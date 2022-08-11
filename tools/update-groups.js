d529bcd3dacd04d11c5b45019ed90ed473629de5
kobakazu0429 <kobakazu10@gmail.com>
2022-08-11 09:56:19 +0900

    fix: isbn range url

--- tools/update-groups.js
+++ tools/update-groups.js
@@ -6,70 +6,55 @@ const fs = require('fs');
 
 // Request a range file
 process.stdout.write('Requesting XML ranges file...\n');
-request.post('https://www.isbn-international.org/?q=bl_proxy/GetRangeInformations',
-  { form: { format: 1, language: 'en', translatedTexts: 'Printed;Last Change' } },
+request.get('http://www.isbn-international.org/export_rangemessage.xml',
   function (error, response, body) {
+  if (error) {
+    throw error;
+  }
+
+  // Parse XML response
+  parseString(body, function (error, result) {
     if (error) {
       throw error;
     }
 
-    const json = JSON.parse(body);
-    const value = json.result.value;
-    const fileName = json.result.filename;
-    const fileUrl = `https://www.isbn-international.org/?q=download_range/${value}/${fileName}`;
+    const prefixes = result.ISBNRangeMessage.RegistrationGroups[0].Group;
+    const groups = {};
 
-    // Download the rangefile
-    process.stdout.write(`Downloading ${fileUrl}...\n`);
-    request.get(fileUrl,
-      function (error, response, body) {
-        if (error) {
-          throw error;
-        }
-
-        // Parse XML response
-        parseString(body, function (error, result) {
-          if (error) {
-            throw error;
+    // Extract agency name and ranges from each group
+    for (let i = 0, c = prefixes.length; i < c; i++) {
+      const prefix = prefixes[i];
+      const groupKey = prefix.Prefix[0];
+      const group = {
+        name: prefix.Agency[0],
+        ranges: prefix.Rules[0].Rule.filter(function (rule) {
+          if (rule.Length[0] === '0') {
+            return false;
           }
 
-          const prefixes = result.ISBNRangeMessage.RegistrationGroups[0].Group;
-          const groups = {};
+          return true;
+        }).map(function (rule) {
+          const length = rule.Length[0];
+          const range = rule.Range[0].toString().split('-');
+          const start = range[0].substring(0, length);
+          const end = range[1].substring(0, length);
+          return [start, end];
+        })
+      };
+      groups[groupKey] = group;
+    }
 
-          // Extract agency name and ranges from each group
-          for (let i = 0, c = prefixes.length; i < c; i++) {
-            const prefix = prefixes[i];
-            const groupKey = prefix.Prefix[0];
-            const group = {
-              name: prefix.Agency[0],
-              ranges: prefix.Rules[0].Rule.filter(function (rule) {
-                if (rule.Length[0] === '0') {
-                  return false;
-                }
+    // Stringify object & format string
+    const groupsString = JSON.stringify(groups, null, 2);
+    const content = `module.exports = ${groupsString};`;
 
-                return true;
-              }).map(function (rule) {
-                const length = rule.Length[0];
-                const range = rule.Range[0].toString().split('-');
-                const start = range[0].substring(0, length);
-                const end = range[1].substring(0, length);
-                return [start, end];
-              })
-            };
-            groups[groupKey] = group;
-          }
+    // Write file
+    fs.writeFile('lib/groups.js', content, (err) => {
+      if (err) {
+        throw err;
+      }
 
-          // Stringify object & format string
-          const groupsString = JSON.stringify(groups, null, 2);
-          const content = `module.exports = ${groupsString};`;
-
-          // Write file
-          fs.writeFile('lib/groups.js', content, (err) => {
-            if (err) {
-              throw err;
-            }
-
-            process.stdout.write('Updated file lib/groups.js\n');
-          });
-        });
-      });
+      process.stdout.write('Updated file lib/groups.js\n');
+    });
   });
+});
